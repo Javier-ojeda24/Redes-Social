3a7bffc46775fb54883a24bf209b5c8a8e8e6699
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-02-23 14:59:38 -0300

    maquetando parte de login

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -6,7 +6,26 @@ export const Login = () => {
       <header className="content__header content__header-public">
         <h1 className="content__title">Login</h1>
       </header>
-      <div className="content__posts"></div>
+      <div className="content__posts">
+        <form className="for-login">
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Contraseña</label>
+            <input type="password" name="password" />
+          </div>
+
+          <input
+            type="submit"
+            value="Identificate"
+            className="btn btn-success"
+          />
+          
+        </form>
+      </div>
     </>
   );
 };
