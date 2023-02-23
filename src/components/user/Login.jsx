fe13ad59ea9ef0f4363188593372ebc8745b73fb
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-02-23 15:08:06 -0300

    recoger datos del formulario login

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -1,21 +1,37 @@
 import React from "react";
+import { useForms } from "../../hooks/useForms";
 
 export const Login = () => {
+  const { forms, changed } = useForms({});
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+
+    //Los datos del formulario
+    let userToLogin = forms;
+
+    //La peticion al back 
+
+
+    //Persister los datos en el navegador
+    
+  };
+
   return (
     <>
       <header className="content__header content__header-public">
         <h1 className="content__title">Login</h1>
       </header>
       <div className="content__posts">
-        <form className="for-login">
+        <form className="for-login" onSubmit={loginUser}>
           <div className="form-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" />
+            <input type="email" name="email" onChange={changed} />
           </div>
 
           <div className="form-group">
             <label htmlFor="password">Contraseña</label>
-            <input type="password" name="password" />
+            <input type="password" name="password" onChange={changed} />
           </div>
 
           <input
@@ -23,7 +39,6 @@ export const Login = () => {
             value="Identificate"
             className="btn btn-success"
           />
-          
         </form>
       </div>
     </>
