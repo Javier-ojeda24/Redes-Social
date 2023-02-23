25e1412c5113df26bfa06b8b19a4115b8557be75
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-02-23 14:53:59 -0300

    alerta css

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -1,20 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { Global } from "../../helpers/Glogal";
 import { useForms } from "../../hooks/useForms";
 
 export const Register = () => {
   const { forms, changed } = useForms({});
+  const [saved, setSaved] = useState("not-sended");
 
-  const saveUser = (e) => {
+  const saveUser = async (e) => {
+    //prevenir actualizacion de pantalla
     e.preventDefault();
+
+    //Recoger datos del formulario
     let newUser = forms;
-    console.log(newUser);
-  };
+
+    //Guardar usuario en el back
+    const request = await fetch(Global.url + "user/register", {
+      method: "POST",
+      body: JSON.stringify(newUser),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await request.json();
+    if (data.status === "success") {
+      setSaved("saved");
+    } else {
+      setSaved("error");
+    }
+  }; //fin del metodo de guardar
   return (
     <>
       <header className="content__header content__header-public">
         <h1 className="content__title">Registro</h1>
       </header>
       <div className="content__posts">
+        {saved === "saved" ? (
+          <strong className="alert alert-success">
+            Usuario registrado correctamente !!{" "}
+          </strong>
+        ) : (
+          ""
+        )}
+
+        {saved === "error" ? (
+          <strong className="alert alert-danger">
+            Usuario no se ha registrado !!
+          </strong>
+        ) : (
+          ""
+        )}
+
         <form className="register-form" onSubmit={saveUser}>
           <div className="form-group">
             <label htmlFor="name">Nombre</label>
