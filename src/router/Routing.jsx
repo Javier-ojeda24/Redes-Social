d7e6a02ac1dc1f713b0d73b314c7732607da8823
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-02-23 23:34:08 -0300

    componente de ajuste y su ruta

--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -3,8 +3,10 @@ import { BrowserRouter, Route, Routes, Navigate, Link } from "react-router-dom";
 import { PrivateLayout } from "../components/layout/general/PrivateLayout";
 import { PublicLayout } from "../components/layout/public/PublicLayout";
 import { Feed } from "../components/publication/Feed";
+import { Config } from "../components/user/Config";
 import { Login } from "../components/user/Login";
 import { Logout } from "../components/user/Logout";
+import { People } from "../components/user/People";
 import { Register } from "../components/user/Register";
 import { AuthProvider } from "../context/AuthProvider";
 
@@ -23,6 +25,8 @@ export const Routing = () => {
             <Route index element={<Feed />} />
             <Route path="feed" element={<Feed />} />
             <Route path="logout" element={<Logout />} />
+            <Route path="gente" element={<People />} />
+            <Route path="ajustes" element={<Config />} />
           </Route>
           <Route
             path="*"
