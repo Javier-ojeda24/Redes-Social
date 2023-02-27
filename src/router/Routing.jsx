5028a73cef853ce07966eaa66d8f716fcee6c636
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-02-27 18:49:57 -0300

    nueva ruta  y comnponente de publicaciones

--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -9,6 +9,7 @@ import { Config } from "../components/user/Config";
 import { Login } from "../components/user/Login";
 import { Logout } from "../components/user/Logout";
 import { People } from "../components/user/People";
+import { Profile } from "../components/user/Profile";
 import { Register } from "../components/user/Register";
 import { AuthProvider } from "../context/AuthProvider";
 
@@ -31,6 +32,7 @@ export const Routing = () => {
             <Route path="ajustes" element={<Config />} />
             <Route path="siguiendo/:userId" element={<Following />} />
             <Route path="siguidores/:userId" element={<Follower />} />
+            <Route path="perfil/:userId" element={<Profile />} />
           </Route>
           <Route
             path="*"
